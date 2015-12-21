f8ca8e8da2d984b6ba1f0b8aaf37a4894fd5414c
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-21 19:35:53 +0100

    init

---
+++ test.js
@@ -0,0 +1,70 @@
+import test from 'ava';
+import delay from 'delay';
+import fn from './';
+
+test('memoize', t => {
+	let i = 0;
+	const f = () => i++;
+	const memoized = fn(f);
+	t.is(memoized(), 0);
+	t.is(memoized(), 0);
+	t.is(memoized(), 0);
+	t.is(memoized('foo'), 1);
+	t.is(memoized('foo'), 1);
+	t.is(memoized('foo'), 1);
+});
+
+test('memoize with multiple non-primitive arguments', t => {
+	let i = 0;
+	const memoized = fn(() => i++);
+	t.is(memoized(), 0);
+	t.is(memoized(), 0);
+	t.is(memoized({foo: true}, {bar: false}), 1);
+	t.is(memoized({foo: true}, {bar: false}), 1);
+});
+
+test('maxAge option', async t => {
+	let i = 0;
+	const f = () => i++;
+	const memoized = fn(f, {maxAge: 100});
+	t.is(memoized(1), 0);
+	t.is(memoized(1), 0);
+	await delay(50);
+	t.is(memoized(1), 0);
+	await delay(200);
+	t.is(memoized(1), 1);
+});
+
+test('cacheKey option', t => {
+	let i = 0;
+	const f = () => i++;
+	const memoized = fn(f, {cacheKey: x => x});
+	t.is(memoized(1), 0);
+	t.is(memoized(1), 0);
+	t.is(memoized(1, 2), 0);
+	t.is(memoized(2), 1);
+	t.is(memoized(2, 1), 1);
+});
+
+test('cache option', t => {
+	let i = 0;
+	const f = () => i++;
+	const memoized = fn(f, {
+		cache: new WeakMap(),
+		cacheKey: x => x
+	});
+	const foo = {};
+	const bar = {};
+	t.is(memoized(foo), 0);
+	t.is(memoized(foo), 0);
+	t.is(memoized(bar), 1);
+	t.is(memoized(bar), 1);
+});
+
+test('promise support', async t => {
+	let i = 0;
+	const memoized = fn(() => Promise.resolve(i++));
+	t.is(await memoized(), 0);
+	t.is(await memoized(), 0);
+	t.is(await memoized(10), 1);
+});
