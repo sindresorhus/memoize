493f6c959a1820573642e5aba0b5fc1d577fd186
Dylan Greene <dgreene@medallia.com>
2019-03-13 20:38:26 -0400

    update tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -7,10 +7,10 @@ expectType<(string: string) => boolean>(mem(fn));
 expectType<(string: string) => boolean>(mem(fn, {maxAge: 1}));
 expectType<(string: string) => boolean>(mem(fn, {cacheKey: (...arguments_) => arguments_}));
 expectType<(string: string) => boolean>(
-	mem(fn, {cacheKey: (...arguments_) => arguments_, cache: new Map<[string], boolean>()})
+	mem(fn, {cacheKey: (...arguments_) => arguments_, cache: new Map<[string], { data: boolean }>()})
 );
 expectType<(string: string) => boolean>(
-	mem(fn, {cache: new Map<[string], boolean>()})
+	mem(fn, {cache: new Map<[string], { data: boolean; maxAge?: number }>()})
 );
 expectType<(string: string) => boolean>(mem(fn, {cachePromiseRejection: true}));
 
