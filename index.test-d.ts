719fd10d72bbdfd03b01d0d6d8e0aa095b979952
Federico <me@fregante.com>
2020-10-03 02:31:22 -0500

    Try index.test-d.ts from #66

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,7 +1,7 @@
 import {expectType} from 'tsd';
 import mem = require('.');
 
-const fn = (string: string) => true;
+const fn = (_string: string) => true;
 
 expectType<typeof fn>(mem(fn));
 expectType<typeof fn>(mem(fn, {maxAge: 1}));
@@ -9,11 +9,44 @@ expectType<typeof fn>(mem(fn, {cacheKey: (...arguments_) => arguments_}));
 expectType<typeof fn>(
 	mem(
 		fn,
-		{cacheKey: (arguments_) => arguments_,
-		cache: new Map<[string], {data: boolean; maxAge: number}>()})
+		{
+			cacheKey: arguments_ => arguments_[0],
+			cache: new Map<string, {data: boolean; maxAge: number}>()
+		}
+	)
 );
 expectType<typeof fn>(
-	mem(fn, {cache: new Map<[string], {data: boolean; maxAge: number}>()})
+	mem(fn, {cache: new Map<string, {data: boolean; maxAge: number}>()})
+);
+
+// Testing that the full cache object works with type inference
+function objectFnReturnsNumber(_args: {key: string}) {
+	return 10;
+}
+
+expectType<typeof objectFnReturnsNumber>(
+	mem(
+		objectFnReturnsNumber,
+		{
+			cacheKey: (_args: [{key: string}]) => {
+				return new Date();
+			},
+			cache: {
+				get: (_key: Date) => {
+					return {
+						data: 5,
+						maxAge: 2
+					};
+				},
+				set: (_key: Date, _value: { data: number; maxAge: number }) => {},
+				has: (_key: Date) => {
+					return true;
+				},
+				delete: (_key: Date) => {},
+				clear: () => {}
+			}
+		}
+	)
 );
 
 /* Overloaded function tests */
@@ -22,6 +55,7 @@ function overloadedFn(parameter: true): true;
 function overloadedFn(parameter: boolean): boolean {
 	return parameter;
 }
+
 expectType<typeof overloadedFn>(mem(overloadedFn));
 expectType<true>(mem(overloadedFn)(true));
 expectType<false>(mem(overloadedFn)(false));
