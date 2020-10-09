514db44e68b4b80686823a1b3ee492a79a679244
Owen Allen <owenallenaz@gmail.com>
2020-10-09 13:21:27 -0700

    Fixes cacheKey typescript mechanics.

--- test-d/index.test-d.ts
+++ test-d/index.test-d.ts
@@ -5,7 +5,7 @@ const fn = (text: string) => Boolean(text);
 
 expectType<typeof fn>(mem(fn));
 expectType<typeof fn>(mem(fn, {maxAge: 1}));
-expectType<typeof fn>(mem(fn, {cacheKey: ([firstArgument]) => firstArgument}));
+expectType<typeof fn>(mem(fn, {cacheKey: ([firstArgument]: [string]) => firstArgument}));
 expectType<typeof fn>(
 	mem(fn, {
 		// The cacheKey returns an array. This isn't deduplicated by a regular Map, but it's valid. The correct solution would be to use ManyKeysMap to deduplicate it correctly
@@ -35,14 +35,44 @@ mem.clear(fn);
 // The argument should match the memoized function’s parameters
 mem((text: string) => Boolean(text), {
 	cacheKey: arguments_ => {
-		// @ts-expect-error #50
 		expectType<[string]>(arguments_);
 	}
 });
 
 mem(() => 1, {
 	cacheKey: arguments_ => {
-		// @ts-expect-error #50
 		expectType<[]>(arguments_);
+	}
+});
+
+// Ensures that the various cache functions infer their arguments type from the return type of cacheKey()
+mem((_args: {key: string}) => 1, {
+	cacheKey: (arguments_: [{key: string}]) => {
+		expectType<[{key: string}]>(arguments_);
+		return new Date();
+	},
+	cache: {
+		get: key => {
+			expectType<Date>(key);
+
+			return {
+				data: 5,
+				maxAge: 2
+			};
+		},
+		set: (key, data) => {
+			expectType<Date>(key);
+			expectType<{data: number; maxAge: number}>(data);
+		},
+		has: key => {
+			expectType<Date>(key);
+			return true;
+		},
+		delete: key => {
+			expectType<Date>(key);
+		},
+		clear: () => {
+			return undefined;
+		}
 	}
 });
